4e18048d7e9e08c4286580634b1f17b5fe7f5a38
Jhonata Rios <72048481+jhonatarios@users.noreply.github.com>
2022-09-12 17:28:50 -0300

    aula 3

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 
 function App() {
+
+  const name = "Jhonata"
+
+  const newName = name.toUpperCase()
+
+  function sum(a, b){
+    return a + b
+  }
+
+  const url = "https://via.placeholder.com/150"
+
   return (
     <div className="App">
-      <h1>Olá React!</h1>
-      <p>Meu primeiro App</p>
+      <h2>Alterando o JSX</h2>
+      <p>Olá, {newName}</p>
+      <p>Soma: {sum(1, 2)}</p>
+      <img src={url} alt="Minha imagem" />
     </div>
   );
 }
